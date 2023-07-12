20d59989ec3d2dc17a6f776aed0af244a56be27d
Otávio <otavio.rodf@gmail.com>
2023-07-12 10:34:26 -0300

    fix:  fixed model and alembic migrations

--- app/entities/models.py
+++ app/entities/models.py
@@ -1,11 +1,11 @@
 import datetime
 
-from sqlalchemy import Column, Integer, String, datetime
+from sqlalchemy import Column, Integer, String, DateTime
 from .database import Base
 
 class History(Base):
     __tablename__ = "history"
     id = Column(Integer, primary_key= True, index= True)
-    query = Column(String)
-    result = Column(String)
+    query = Column(String(length=230))
+    result = Column(String(length=20))
     created_at = Column(DateTime, default=datetime.datetime.utcnow)
